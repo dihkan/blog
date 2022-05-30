dc0702a1e84fb55d8617b0de1d2148f42beeeb47
ihsan dihkan <dihkan@gmail.com>
2022-05-30 17:35:26 +0300

    dotEnv

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -3,7 +3,11 @@
     class="bg-zinc-900 p-5 text-zinc-900 w-1/6 min-h-full flex-col divide-x-4 border-sky-50"
   >
     <div class="flex items-start justify-center w-full pt-2">
-      <img src="../assets/img/ihsan.jpeg" class="rounded-full w-48" alt="" />
+      <img
+        :src="`${this.baseUrl}/img/ihsan.jpeg`"
+        class="rounded-full w-48"
+        alt=""
+      />
     </div>
     <router-link to="/" tag="li" :class="linkClasses">
       <a href="#" class="aClasses">
@@ -17,7 +21,7 @@
       >
     </router-link>
     <router-link to="/currency" :class="linkClasses">
-      <a href="#" class="aClasses">
+      <a href="#" :class="aClasses">
         <el-icon size="20px"><TrendCharts /></el-icon>
         Currency</a
       >
@@ -29,6 +33,9 @@
 import { ref } from "vue";
 const linkClasses = ref("w-full bg-sky-700 inline-block p-3 mt-6 rounded-md");
 const aClasses = ref("mt-5  font-bold");
+const baseUrl = ref();
+baseUrl.value =
+  process.env.NODE_ENV === "production" ? "assets" : "/src/assets";
 </script>
 
 <style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import dotEnv from "dotenv";
 import elementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
@@ -9,7 +10,9 @@ import "./assets/index.css";
 import "./assets/genel.css";
 
 const app = createApp(App);
+app.use(dotEnv);
 app.use(elementPlus);
+
 app.use(router);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component);
